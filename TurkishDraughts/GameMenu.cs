0d1cc4027c9ae52d7738845860a42890c6a7a9a6
RaduCruceat <radu.cruceat@gmail.com>
2024-03-22 01:02:25 +0200

    Add project files.

---
+++ TurkishDraughts/GameMenu.cs
@@ -0,0 +1,38 @@
+namespace TurkishDraughts
+{
+    public partial class GameMenu : Form
+    {
+        
+        public GameMenu()
+        {
+            InitializeComponent();
+            MaximizeBox= false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isopen = false;
+            foreach (Form f in Application.OpenForms)//verifica elementele deschise din aplicatie
+            {
+                if (f.Name == "GameBoard")
+                {
+                    isopen = true;
+                    f.BringToFront();//se selecteaza ultimul forms deschis ca principal
+                    break;
+                }
+            }
+            if (isopen == false)
+            {
+                GameBoard gameBoard = new GameBoard();
+                gameBoard.Show();
+                this.textBox1.ForeColor = Color.Red;
+                    
+            }
+        }
+    }
+}
---
+++ TurkishDraughts/PieceClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurkishDraughts.Properties;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Drawing;
+using System.Xml.Serialization;
+
+
+
+namespace TurkishDraughts
+{
+    internal class PieceClass
+    {
+        private int i, j, value;
+        public PictureBox pictureBoxButtons;
+        protected GameBoard gameBoard;
+
+        public PieceClass(int i, int j, int value, GameBoard gameBoard)
+        {
+            this.gameBoard = gameBoard;
+            this.i = i;
+            this.j = j;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameBoard));
+            Image tempRes = ((System.Drawing.Image)(resources.GetObject("EmptySquare"))); //imagine default
+            if (i == 1 || i == 2)
+            {
+                tempRes = Resources.BlackPiece;
+                value = 1;
+            }
+            if (i == 5 || i == 6)
+            {
+                tempRes = Resources.RedPiece;
+                value = 2;
+            }
+            if (i == 0 || i == 7 || i > 2 && i < 5)
+            {
+                tempRes = Resources.EmptySquare;
+                value = 0;
+            }
+
+        }
+    }
+}
